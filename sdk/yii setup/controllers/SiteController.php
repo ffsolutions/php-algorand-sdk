3d6c7aabba28aa2f3b769265994055eb83e97182
FF Solutions <63802942+ffsolutions@users.noreply.github.com>
2021-04-08 21:16:08 -0300

    Compatibility updates

--- sdk/yii setup/controllers/SiteController.php
+++ sdk/yii setup/controllers/SiteController.php
@@ -76,11 +76,13 @@ class SiteController extends Controller
     public function actionAlgod()
     {
 
-        $algorand = new algod('4820e6e45f339e0026eaa2b74c2aa7d8735cbcb2db0cf0444fb492892e1c09b7',"localhost",53898);
+        $algorand = new algod('{algod-token}',"localhost",53898);
         $algorand->debug(1);
 
         #Gets the current node status.
         $return=$algorand->get("v2","status");
+
+        #See all Algorand SDK Functions at: https://github.com/ffsolutions/php-algorand-sdk
 
         #Full response with debug (json response)
         if(!empty($return)){
@@ -104,12 +106,13 @@ class SiteController extends Controller
     public function actionKmd()
     {
 
-        $algorand_kmd = new kmd('dcb406527f3ded8464dbd56e6ea001b9b17882cfcf8194c17069bb22816307ad',"localhost",7833);
+        $algorand_kmd = new kmd('{kmd-token}',"localhost",7833);
         $algorand_kmd->debug(1);
 
         #Get Versions
         $return=$algorand_kmd->get("versions");
 
+        #See all Algorand SDK Functions at: https://github.com/ffsolutions/php-algorand-sdk
 
         #Full response with debug (json response)
         if(!empty($return)){
